1186dccfddcc546d54af430ed68f8ce7fc2e1e8b
kimjihun <kimjihun-dev@gmail.com>
2020-06-30 16:57:29 +0900

    Updates

---
+++ precache-manifest.4b133163dd28b4ea8bdb1225b8749030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8473a6d0ee24c704920a6f763aadbc6f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b9ace478de5cc7e79b3d",
+    "url": "/movie_app/static/css/main.ccc184c7.chunk.css"
+  },
+  {
+    "revision": "60909df7602afad9eb2a",
+    "url": "/movie_app/static/js/2.c90c8fdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.c90c8fdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ace478de5cc7e79b3d",
+    "url": "/movie_app/static/js/main.407676f7.chunk.js"
+  },
+  {
+    "revision": "59359fbc215704906760",
+    "url": "/movie_app/static/js/runtime-main.153ee399.js"
+  }
+]);
